7a647a34c024f687fb4bc158ddfb27b5c6747040
Gianluca <gzuccare@redhat.com>
2020-12-28 12:16:23 +0000

    feat: add logging

--- app/src/main/java/com/aquatic/lucre/MainActivity.kt
+++ app/src/main/java/com/aquatic/lucre/MainActivity.kt
@@ -2,10 +2,13 @@ package com.aquatic.lucre
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AnkoLogger {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        info("App started!")
     }
 }

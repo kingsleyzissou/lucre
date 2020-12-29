a45d89e0cbc2981dec320b92dd184f8fbade406e
Gianluca <gzuccare@redhat.com>
2020-12-29 19:32:03 +0000

    feat: add click listener to vault list items

--- app/src/main/java/com/aquatic/lucre/activities/VaultListActivity.kt
+++ app/src/main/java/com/aquatic/lucre/activities/VaultListActivity.kt
@@ -6,11 +6,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.aquatic.lucre.R
 import com.aquatic.lucre.adapters.VaultAdapter
+import com.aquatic.lucre.adapters.VaultListener
 import com.aquatic.lucre.main.App
+import com.aquatic.lucre.models.Vault
 import kotlinx.android.synthetic.main.activity_vault_list.* // ktlint-disable no-wildcard-imports
+import org.jetbrains.anko.intentFor
 import org.jetbrains.anko.startActivityForResult
+import org.jetbrains.anko.toast
 
-class VaultListActivity : AppCompatActivity() {
+class VaultListActivity : AppCompatActivity(), VaultListener {
     lateinit var app: App
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,13 +25,19 @@ class VaultListActivity : AppCompatActivity() {
         toolbar.title = title
         setSupportActionBar(toolbar)
 
+        toast("Store size: ${app.vaultStore.list.size}")
+
         val layoutManager = LinearLayoutManager(this)
         recyclerView.layoutManager = layoutManager
-        recyclerView.adapter = VaultAdapter(app.vaultStore.all())
+        recyclerView.adapter = VaultAdapter(app.vaultStore.all(), this)
+    }
+
+    override fun onVaultClick(Vault: Vault) {
+        startActivityForResult(intentFor<VaultActivity>(), 0)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_vault, menu)
+        menuInflater.inflate(R.menu.menu_vault_list, menu)
         return super.onCreateOptionsMenu(menu)
     }
 
--- app/src/main/java/com/aquatic/lucre/adapters/VaultAdapter.kt
+++ app/src/main/java/com/aquatic/lucre/adapters/VaultAdapter.kt
@@ -8,7 +8,14 @@ import com.aquatic.lucre.R
 import com.aquatic.lucre.models.Vault
 import kotlinx.android.synthetic.main.card_vault.view.*
 
-class VaultAdapter constructor(private var vaults: List<Vault>) : RecyclerView.Adapter<VaultAdapter.MainHolder>() {
+interface VaultListener {
+    fun onVaultClick(Vault: Vault)
+}
+
+class VaultAdapter constructor(
+    private var vaults: List<Vault>,
+    private val listener: VaultListener
+) : RecyclerView.Adapter<VaultAdapter.MainHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VaultAdapter.MainHolder {
         return MainHolder(
@@ -22,16 +29,18 @@ class VaultAdapter constructor(private var vaults: List<Vault>) : RecyclerView.A
 
     override fun onBindViewHolder(holder: VaultAdapter.MainHolder, position: Int) {
         val vault = vaults[holder.adapterPosition]
-        holder.bind(vault)
+        holder.bind(vault, listener)
     }
 
     override fun getItemCount(): Int = vaults.size
 
     class MainHolder constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-        fun bind(vault: Vault) {
+        fun bind(vault: Vault, listener: VaultListener ) {
             itemView.title.text = vault.name
             itemView.description.text = vault.description
+//            itemView.currency.text = vault.currency
+            itemView.setOnClickListener { listener.onVaultClick(vault) }
         }
     }
 }

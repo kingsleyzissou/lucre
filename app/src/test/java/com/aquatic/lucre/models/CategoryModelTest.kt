ed8ec8c00f030c56b07f7dae3b1a8fe5325e8857
Gianluca <gzuccare@redhat.com>
2020-12-30 17:38:42 +0000

    test: import model tests & convert to junit4

---
+++ app/src/test/java/com/aquatic/lucre/models/CategoryModelTest.kt
@@ -0,0 +1,59 @@
+package com.aquatic.lucre.models
+
+import org.junit.Assert.* // ktlint-disable no-wildcard-imports
+import org.junit.Test
+import org.junit.Before
+
+internal class CategoryModelTest {
+
+    lateinit var category: Category
+
+    private final val EXPECTED_NAME = "Takeout"
+    private final val EXPECTED_DESCRIPTION = "Dine-in and takeaways"
+    private final val EXPECTED_COlOR = "#ff0000"
+
+    @Before
+    internal fun setup() {
+        category = Category(
+            "Takeout",
+            "Dine-in and takeaways",
+            "#ff0000"
+        )
+    }
+
+    @Test
+    fun getName() {
+        assertEquals(EXPECTED_NAME, category.name)
+    }
+
+    @Test
+    fun setName() {
+        val newName = "Bills"
+        category.name = newName
+        assertEquals(newName, category.name)
+    }
+
+    @Test
+    fun getDescription() {
+        assertEquals(EXPECTED_DESCRIPTION, category.description)
+    }
+
+    @Test
+    fun setDescription() {
+        val newDescription = "Utilities and lights"
+        category.description = newDescription
+        assertEquals(newDescription, category.description)
+    }
+
+    @Test
+    fun getColor() {
+        assertEquals(EXPECTED_COlOR, category.color)
+    }
+
+    @Test
+    fun setColor() {
+        val newColor = "#00ff00"
+        category.color = newColor
+        assertEquals(newColor, category.color)
+    }
+}

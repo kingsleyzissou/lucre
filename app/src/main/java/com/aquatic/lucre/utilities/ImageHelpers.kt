0e13d2900072308d2e811d0ba3601489754b42de
Gianluca <gzuccare@redhat.com>
2020-12-31 10:54:30 +0000

    feat: add image helpers

---
+++ app/src/main/java/com/aquatic/lucre/utilities/ImageHelpers.kt
@@ -0,0 +1,35 @@
+package com.aquatic.lucre.utilities
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import androidx.core.app.ActivityCompat.startActivityForResult
+import com.aquatic.lucre.R
+
+fun showImagePicker(parent: Activity, id: Int) {
+    val intent = Intent()
+    intent.type = "image/*"
+    intent.action = Intent.ACTION_OPEN_DOCUMENT
+    intent.addCategory(Intent.CATEGORY_OPENABLE)
+    val chooser = Intent.createChooser(intent, R.string.select_entry_image.toString())
+    parent.startActivityForResult(chooser, id)
+}
+
+fun readImage(context: Context, path: String?): Bitmap? {
+    var bitmap: Bitmap? = null
+    val uri = Uri.parse(path)
+    if (uri != null) {
+        try {
+            val parcelFileDescriptor = context.getContentResolver().openFileDescriptor(uri, "r")
+            val fileDescriptor = parcelFileDescriptor?.getFileDescriptor()
+            bitmap = BitmapFactory.decodeFileDescriptor(fileDescriptor)
+            parcelFileDescriptor?.close()
+        } catch (e: Exception) {
+            error(e)
+        }
+    }
+    return bitmap
+}

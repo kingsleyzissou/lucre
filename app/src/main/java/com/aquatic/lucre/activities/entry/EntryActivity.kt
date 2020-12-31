c242e6c4a6fabf86488e4dc94d882121b0d439f9
Gianluca <gzuccare@redhat.com>
2020-12-31 10:55:04 +0000

    feat: add image chooser to entry

--- app/src/main/java/com/aquatic/lucre/activities/entry/EntryActivity.kt
+++ app/src/main/java/com/aquatic/lucre/activities/entry/EntryActivity.kt
@@ -1,5 +1,6 @@
 package com.aquatic.lucre.activities.entry
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
@@ -11,9 +12,15 @@ import com.aquatic.lucre.main.App
 import com.aquatic.lucre.models.Category
 import com.aquatic.lucre.models.Entry
 import com.aquatic.lucre.models.EntryType
+import com.aquatic.lucre.utilities.readImage
+import com.aquatic.lucre.utilities.showImagePicker
 import kotlinx.android.synthetic.main.activity_entry.*
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
 
-class EntryActivity : AppCompatActivity() {
+val IMAGE_REQUEST = 1
+
+class EntryActivity : AppCompatActivity(), AnkoLogger {
 
     var entry = Entry()
     var vault: String? = null
@@ -43,13 +50,24 @@ class EntryActivity : AppCompatActivity() {
             type.setSelectedItem(entry.type.toString().toLowerCase())
             entryDescription.setText(entry.description)
             category.setSelectedItem(entry.category!!)
+            if (entry.image.isNotEmpty()) {
+                val bitmap = readImage(this, entry.image)
+                info("$bitmap")
+                entryImage.setImageBitmap(bitmap)
+                entryImageButton.setText(R.string.entry_edit_image)
+            }
             entrySubmit.setText(R.string.item_edit)
         }
 
         entryAddToolbar.title = title
         setSupportActionBar(entryAddToolbar)
 
+        entryImageButton.setOnClickListener { selectImage() }
         entrySubmit.setOnClickListener { submit() }
+    }
+
+    private fun selectImage() {
+        showImagePicker(this, IMAGE_REQUEST)
     }
 
     private fun submit() {
@@ -69,6 +87,24 @@ class EntryActivity : AppCompatActivity() {
         val message = getResources().getString(R.string.required)
         return entryAmount.validate(message) { it.isNotEmpty() } &&
             entryVendor.validate(message) { it.isNotEmpty() }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            IMAGE_REQUEST -> {
+                if (data != null) {
+                    // permissions issues
+                    // https://developer.android.com/training/data-storage/shared/documents-files#persist-permissions
+                    val takeFlags: Int = Intent.FLAG_GRANT_READ_URI_PERMISSION or
+                        Intent.FLAG_GRANT_WRITE_URI_PERMISSION
+                    // Check for the freshest data.
+                    contentResolver.takePersistableUriPermission(data.getData()!!, takeFlags)
+                    entry.image = data.getData().toString()
+                    entryImage.setImageURI(data.getData())
+                }
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {

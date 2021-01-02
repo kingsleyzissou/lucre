83425c4593be639e60518a9ca5f00ba7eaec4271
Gianluca <gzuccare@redhat.com>
2021-01-02 16:02:35 +0000

    refactor: add base adapter

--- app/src/main/java/com/aquatic/lucre/adapters/CategoryAdapter.kt
+++ app/src/main/java/com/aquatic/lucre/adapters/CategoryAdapter.kt
@@ -3,15 +3,14 @@ package com.aquatic.lucre.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
 import com.aquatic.lucre.R
 import com.aquatic.lucre.models.Category
 import kotlinx.android.synthetic.main.card_category.view.*
 
 class CategoryAdapter constructor(
-    private var categories: List<Category>,
-    private val listener: AdapterListener<Category>
-) : RecyclerView.Adapter<CategoryAdapter.MainHolder>() {
+    categories: MutableList<Category>,
+    listener: AdapterListener<Category>
+) : BaseAdapter<Category>(categories, listener) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryAdapter.MainHolder {
         return CategoryAdapter.MainHolder(
@@ -23,18 +22,11 @@ class CategoryAdapter constructor(
         )
     }
 
-    override fun onBindViewHolder(holder: MainHolder, position: Int) {
-        val category = categories[position]
-        holder.bind(category, listener)
-    }
-
-    override fun getItemCount(): Int = categories.size
-
-    class MainHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bind(category: Category, listener: AdapterListener<Category>) {
-            itemView.categoryTitle.text = category.name
-            itemView.categoryDescription.text = category.description
-            itemView.setOnClickListener { listener.onCardClick(category) }
+    class MainHolder constructor(itemView: View) : BaseAdapter.MainHolder<Category>(itemView) {
+        override fun bind(value: Category, listener: AdapterListener<Category>) {
+            itemView.categoryTitle.text = value.name
+            itemView.categoryTitle.text = value.name
+            itemView.categoryTitle.text = value.name
         }
     }
 }

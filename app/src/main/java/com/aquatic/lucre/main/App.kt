d10f697cd54f5bd6f543050e5738a596d4eb7ed2
Gianluca <gzuccare@redhat.com>
2021-01-02 16:05:00 +0000

    refactor: remove store creation from app object

--- app/src/main/java/com/aquatic/lucre/main/App.kt
+++ app/src/main/java/com/aquatic/lucre/main/App.kt
@@ -12,68 +12,9 @@ import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 
 class App : Application(), AnkoLogger {
-
     var user: User? = null
-    lateinit var firestore: FirebaseFirestore
-    lateinit var vaultStore: VaultStore
-    lateinit var categoryStore: CategoryStore
-    lateinit var entryStore: EntryStore
-
-    private val vault1 = Vault(
-        "AIB",
-        "Euro",
-        "€"
-    )
-
-    private val vault2 = Vault(
-        "US",
-        "Dollar",
-        "$"
-    )
-
-    private val vault3 = Vault(
-        "HSBC",
-        "Pound",
-        "£"
-    )
-
-    private val c1 = Category(
-        "Bills",
-        ":(",
-        "#FFFFFF"
-    )
-
-    private val c2 = Category(
-        "Rent",
-        ":(",
-        "#FF0000"
-    )
-
-    private val c3 = Category(
-        "Salary",
-        ":)",
-        "#0000FF"
-    )
-
     override fun onCreate() {
         super.onCreate()
-        firestore = FirebaseFirestore.getInstance()
-        vaultStore = VaultStore(firestore.collection("vaults"))
-        categoryStore = CategoryStore(firestore.collection("categories"))
-        entryStore = EntryStore(firestore.collection("entries"))
-//        vaultStore.addAll(listOf(vault1, vault2, vault3))
-//        categoryStore.addAll(listOf(c1, c2, c3))
-//        entryStore.create(
-//            Entry(
-//                20f,
-//                EntryType.EXPENSE,
-//                "Vodafone",
-//                "Phone bill",
-//                c1,
-//                vault2.id
-//            )
-//        )
         info { "Lucre App started" }
     }
-
 }
